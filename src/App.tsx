8f17fdb8e439aa7e3d30b69f07de6dee7b77c342
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-08-23 22:29:37 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import './App.css'
+import bg from "./assets/site.jpg"
+
 
 function App() {
 
   return (
     <>
-     
-
-
-<div className="overlay"></div>
-<div className="stars" aria-hidden="true"></div>
-<div className="starts2" aria-hidden="true"></div>
-<div className="stars3" aria-hidden="true"></div>
-<main className="main">
-	<section className="contact">
-		<h1 className="title">Site Under Construction</h1>
-		<h2 className="sub-title">Email: office@onme.agency</h2>
-    <h2 className="sub-title2">Mobile: +995 596 91 91 91</h2>
-	</section>
-</main>
+    <img src={bg} alt='bg'  className='bg'/>
     </>
   )
 }
